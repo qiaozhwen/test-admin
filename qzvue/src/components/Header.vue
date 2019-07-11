457c75d8dbe71a336e70bd537cdfeb5cbfb4a481
qiaozhwen <1197433123@qq.com>
2019-07-11 18:33:24 +0800

    0711

---
+++ qzvue/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+  .header{
+    width: 100%;
+    height: 140px;
+    background: #464646;
+  }
+</style>
